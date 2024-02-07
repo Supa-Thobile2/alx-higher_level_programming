18a2ca22a2aced66435f3ee73832ea1e62ccb77b
Supa-Thobile2 <thobilenontenja198@gmail.com>
2024-02-07 15:08:54 -0800

    updated projrct

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -4,5 +4,5 @@ word_first_3 = word[:3]
 word_last_2 = word[:2]
 middle_word = word[1:-1]
 print("The first 3 letters:{}".format(word_first_3))
-print("The last letters:{}".format(word_last_2))
+print("The last 2 letters:{}".format(word_last_2))
 print("Middle word:{}".format(middle_word))
