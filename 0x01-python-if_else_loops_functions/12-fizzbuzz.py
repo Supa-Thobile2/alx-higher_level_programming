a0fa7c6087d390575053de33308051106ce7e04f
Supa-Thobile2 <thobilenontenja198@gmail.com>
2024-02-09 12:29:13 -0800

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Author - Thobile
 """Print the number fromm 1 to 100 seperated by a space"""
-def fizzbuzz():
+def fizzbuzz():  
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
