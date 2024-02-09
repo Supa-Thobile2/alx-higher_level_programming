ee9b5508269cb4eea77a66efcb98be3e43f40ef2
Supa-Thobile2 <thobilenontenja198@gmail.com>
2024-02-09 11:31:37 -0800

    7-main.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author - Thobile
+def islower(c):
+    """Function checks for lowrcse characters"""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else: return False
