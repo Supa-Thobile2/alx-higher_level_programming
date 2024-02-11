834eeb8e9083b5130b8bd0d06b26b29480e06c30
Supa-Thobile2 <thobilenontenja198@gmail.com>
2024-02-11 13:29:19 -0800

    updated 7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-word_last_2 = word[:2]
+word_last_2 = word[-2:]
 middle_word = word[1:-1]
 print("The first 3 letters:{}".format(word_first_3))
 print("The last 2 letters:{}".format(word_last_2))
