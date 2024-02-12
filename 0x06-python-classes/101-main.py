b70073c76dda0ac886e060fab043b53ccad7d570
Supa-Thobile2 <thobilenontenja198@gmail.com>
2024-02-12 11:53:12 -0800

    added 101-square.py

---
+++ 0x06-python-classes/101-main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+Square = __import__('101-square').Square
+
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (4, 1))
+print(my_square)
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"101-main.py" 0L, 0C                                          0,0-1         All
+
